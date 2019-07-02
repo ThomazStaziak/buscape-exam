04f81480849d152d76c443f30543d484e844bc75
ThomazStaziak <thostaziak@gmail.com>
2019-07-01 23:55:32 -0300

    adding the toggle menu component

---
+++ app/src/components/ToggleButton/ToggleButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ToggleButton.css'
+
+const ToggleButton = () => {
+    return (
+      <div className="toggle">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+      </div>
+    );
+}
+
+export default ToggleButton
